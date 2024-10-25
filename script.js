e9828910bdc2606677b4c7d7afebfa2756480922
Mehmet Ali <87533107+malitopal113@users.noreply.github.com>
2024-10-25 11:11:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,4 +26,37 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+window.addEventListener('scroll', function() {
+    var topMenu = document.getElementById('top-menu');
+    var navbar = document.getElementById(('navbar-nav'))
+    var scrollPosition = window.scrollY;
+  
+    // Eğer sayfa 100 pikselden fazla kaydırılırsa top-menüyü gizle
+    if (scrollPosition > 70) {
+      topMenu.classList.add('hidden');
+      navbar.classList.add('fixed-top')
+    } else {
+      topMenu.classList.remove('hidden');
+      navbar.classList.remove('fixed-top')
+    }
+  });
 
+// Scroll top butonunu göstermek/gizlemek için
+window.onscroll = function() {
+    var scrollTopBtn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      scrollTopBtn.style.display = "block"; // Sayfa kaydırıldığında göster
+    } else {
+      scrollTopBtn.style.display = "none"; // Sayfanın üstündeyken gizle
+    }
+  };
+  
+  // Scroll top butonuna tıklayınca sayfanın yukarı kayması
+  document.getElementById("scrollTopBtn").addEventListener("click", function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  
